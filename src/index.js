430e5ba57ea29d356439f29c8886311ea371e5b7
Fester28 <pastpa28@yandex.ru>
2020-10-03 19:13:35 +0300

    init: start Typical Arrays Problems- task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array = null) {
+    if (array === null) return 0;
+    if (array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array.sort((a, b) => {
+        return a - b;
+    })[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array = null) {
+    if (array === null) return 0;
+    if (array.length === 0) return 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return array.sort((a, b) => {
+        return a - b;
+    })[array.length - 1];
+};
+
+exports.avg = function avg(array = null) {
+    if (array === null) return 0;
+    if (array.length === 0) return 0;
+
+    let res = 0;
+    let arrL = array.length;
+
+    array.forEach((element) => {
+        res += element;
+    });
+
+    return res / arrL;
+};
